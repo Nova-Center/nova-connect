9a20e8e71e24c2e8d1a3bede2c462500e0f2019b
mahamadou <mahamadou.gory.sio@gmail.com>
2025-06-21 01:45:03 +0200

    feat: create ShopPage component to render ShopList

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,10 @@
+import { ShopList } from "@/components/shop_items/shop-list";
+
+
+export default function ShopPage() {
+  return (
+    <main className="min-h-screen">
+      <ShopList />
+    </main>
+  )
+}
