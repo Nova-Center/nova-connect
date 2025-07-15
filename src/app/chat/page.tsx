1f3ac55f97719e362b7c4d69a44136c1cd849255
mahamadou <mahamadou.gory.sio@gmail.com>
2025-07-15 07:13:53 +0200

    fix: print new component in page.tsx of chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,20 +1,21 @@
+// app/chat/page.tsx
+import ChatList from "@/components/chat/chat-list"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
 import { authOptions } from "../api/auth/[...nextauth]/route"
-import ChatList from "@/components/chat/chat-list"
 
-export default async function EvenementsPage() {
-    const session = await getServerSession(authOptions)
-    if (!session) redirect("/auth/login")
+export default async function ChatListPage() {
+  const session = await getServerSession(authOptions)
+  if (!session) redirect("/auth/login")
 
-    return (
-        <SidebarProvider>
-            <div className="flex min-h-screen w-full">
-                <SidebarInset className="flex-1 bg-zinc-50 dark:bg-zinc-950">
-                    <ChatList />
-                </SidebarInset>
-            </div>
-        </SidebarProvider>
-    )
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen">
+        <SidebarInset className="flex-1 bg-zinc-50">
+          <ChatList />
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
+  )
 }
